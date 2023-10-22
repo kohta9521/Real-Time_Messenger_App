c4b6a1823fa775b726a178335faaa0f79fb47cea
k_95.21 <kohta0421kochi@gmail.com>
2023-10-22 20:44:21 +0900

    add use-react-formの理解がたらない

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 
+// components
+import AuthForm from "./components/AuthForm";
+
 export default function Home() {
     return (
-      <div
-        className="
-            flex
-            min-h-full
-            flex-col
-            justify-center
-            py-12
-            sm:px-6
-            lg:px-8
-            bg-gray-100
-        "
-      >
+        <div
+            className="
+                flex
+                min-h-full
+                flex-col
+                justify-center
+                py-12
+                sm:px-6
+                lg:px-8
+                bg-gray-100
+            "
+        >
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
             <Image
                 alt="Logo"
@@ -35,8 +38,7 @@ export default function Home() {
                 Sign in to your account
             </h2>
         </div>
-        {/* AuthForm */}
-      </div>
+        <AuthForm />
+        </div>
     )
-  }
-  
+}
